f997ffb83d0fe2a21c48e5bf7e5ca0e57ce089b7
Rambh71 <73946122+Rambh71@users.noreply.github.com>
2021-01-21 19:01:15 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,13 @@
+class Form {
+    constructor(){}
+
+    display(){
+        var title= createElement('h2')
+        title.html("Car Racing Game");
+        title.position(130,0);
+
+        var input;
+        var button; 
+        var greeting;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,11 @@
+var back, gameState=0, playerCount, database;
+var form, player, game;
+
+function setup(){
+    createCanvas(400,400);
+    database= firebase.database();
+
+}
+function draw(){
+    
+}
